00606a8d474d95fb918141ed8b30bd39e176e3d2
me-loser <puspendramahariya@gmail.com>
2023-10-28 23:46:44 +0530

    config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,1 @@
-const fs = require("fs");
-const path = require("path");
-
-module.exports = {
-  server: {
-    https: {
-      key: fs.readFileSync(path.join(__dirname, "key.pem")),
-      cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
-    },
-  },
-};
+module.exports = {};
